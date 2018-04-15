4995aca352182c55f9c1968278aa2524815376d5
Shane Lilly <sryanlilly@gmail.com>
2018-04-15 01:17:29 -0500

    Adding IP Scanner from week 2

---
+++ scanner.rb
@@ -0,0 +1,21 @@
+require "ipaddr"
+require "net/ping"
+def calculate_network_range(ip, mask)
+  address = #{ip}/#{mask}"
+  ip_range = []
+  IPAddr.new(address).to_range.map { |ip_object| ip_object.to_s }
+end
+def scan_range(ip_range)
+  active_ips = ip_range.select { |ip| Net::Ping::External.new(ip).ping? }
+end
+def write_to_file(active_ips)
+  File.open("active_ips.txt", "w") do |file|
+    active_ips.each do |ip_address|
+      file.puts ip_address
+    end
+  end
+end
+
+range = calculate_network_range("192.168.1.1", "255.255.255.0")
+active_ips = scan_range(range)
+write_to__file(active_ips)
