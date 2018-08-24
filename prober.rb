8e665f940541fca11cfb76b8dc625251f4f4f663
Shane Lilly <sryanlilly@gmail.com>
2018-08-23 19:40:49 -0500

    Simple prober to check if a user-defined website is up

---
+++ prober.rb
@@ -0,0 +1,33 @@
+# Create a simple prober
+#
+# Poke the Google HTTP endpoint and display the result
+# or instead, ask the user for input
+
+require 'net/http'
+require 'url'
+
+# check http response
+def main(user_input)
+  while true
+    uri = URI.parse(user_input)
+    response = Net::HTTP.get_response(uri)
+    if response.is_a?(Net::HTTPSuccess)
+      puts "Response OK!"
+    else
+      puts "Received #{response.code} code. Probing again in 15s..."
+    end
+    sleep(15)
+  end
+end
+
+# Ask user for a url
+puts "\n\nWhat website would you like to check?\n\n"
+userinput = gets.chomp
+
+
+# Exit on CTRL-C SIGINT
+Signal.trap("INT") {
+  puts "\nUser exited."
+  exit
+}
+main(userinput)
